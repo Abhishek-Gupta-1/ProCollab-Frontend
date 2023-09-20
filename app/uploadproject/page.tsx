e4e6c7579b2aa9ae4d58d2552eb827a52635d20a
Abhishek Gupta <abhisheknew2023@gmail.com>
2023-09-20 21:58:38 +0530

    Commit

--- app/uploadproject/page.tsx
+++ app/uploadproject/page.tsx
@@ -4,12 +4,12 @@ import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 
 export default function UploadProject() {
     return (
-          <div className='flex justify-center '>
+        <div className='flex justify-center '>
 
-         
-        <form >
-            <div className="space-y-12 mt-20">
-                <div className="border-b border-gray-900/10 pb-12">
+
+            <form >
+                <div className="space-y-12 mt-20">
+                    <div className="border-b border-gray-900/10 pb-12">
 
                         <h2 className="text-2xl font-semibold leading-7 text-gray-900">Create Project</h2>
                         {/* <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p> */}
@@ -110,23 +110,23 @@ export default function UploadProject() {
                                 {/* -----------Video Link-------------------------- */}
 
                                 <div className="sm:col-span-4">
-                                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Video Link
-                                </label>
-                                <div className="mt-2">
-                                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">Enter your Project Name</span> */}
-                                        <input
-                                            type="text"
-                                            name="video"
-                                            id="video"
-                                            autoComplete="video"
-                                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                            placeholder="Enter the name of your Project"
-                                        />
+                                    <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Video Link
+                                    </label>
+                                    <div className="mt-2">
+                                        <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                            {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">Enter your Project Name</span> */}
+                                            <input
+                                                type="text"
+                                                name="video"
+                                                id="video"
+                                                autoComplete="video"
+                                                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                placeholder="Enter the name of your Project"
+                                            />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
                                 {/* --------------Image upload------------------ */}
 
