b4ea54fe11eb7aedd81c64dc4115b4a848378ae5
Abhishek Gupta <abhisheknew2023@gmail.com>
2023-09-20 20:35:59 +0530

    Update Upload Page

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -37,7 +37,7 @@ export default function Example() {
                                 >
                                     Search Projects
                                 </a>
-                                <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                <a href="/uploadproject" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                     Upload Projects
                                 </a>
                             </div>
