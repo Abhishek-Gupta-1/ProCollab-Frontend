9e150d6d12aaf278d7d958d59370e11d11bd901e
mehraankush <mehraankush36@gmail.com>
2023-09-22 11:51:15 +0530

    project description page

--- app/searchprojects/page.tsx
+++ app/searchprojects/page.tsx
@@ -2,9 +2,13 @@ import { Search } from "lucide-react"
 import Image from "next/image"
 
 import { projects } from "@/constents"
+import Link from "next/link";
 
 // bg-[#FFFAF0]
 const page = () => {
+
+
+    const productId = 'sdnfsvfd';
   return (
     <div className="flex justify-center bg-[#F5FEFD]  h-screen"> 
         <div className=" md:w-[1024px]">
@@ -22,21 +26,24 @@ const page = () => {
 
             {
                 projects.map((project,i)=>(
+                    
                     <div  key={i} className=" mt-8 mb-5 max-w-xs bg-white  border-gray-200 rounded-lg shadow  dark:border-gray-700">
-                    <div className="w-full">
-                        <a href="#">
-                            <Image className="rounded-t-lg w-full" width={100} height={50} src={project.image} alt="Logo" />
-                        </a>
-                    </div>
-                    <div className="p-5">
-                        <a href="#">
-                            <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900">{project.title}</h5>
-                        </a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{project.shortdescription}</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Read more
-                        </a>
-                    </div>
+                        <Link  href={`/searchprojects/${productId}`}>
+                        <div className="w-full">
+                            <Link href={`/searchprojects/${productId}`}>
+                                <Image className="rounded-t-lg w-full" width={100} height={50} src={project.image} alt="Logo" />
+                            </Link>
+                        </div>
+                        <div className="p-5">
+                            <a href={`/searchprojects/${productId}`}>
+                                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900">{project.title}</h5>
+                            </a>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{project.shortdescription}</p>
+                            <Link href={`/searchprojects/${productId}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Read more
+                            </Link>
+                        </div>
+                </Link>
                </div>
                 ))
             }
