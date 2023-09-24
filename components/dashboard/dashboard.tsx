98171be63344d3e648780726a9999886cfbbda7d
mehraankush <mehraankush36@gmail.com>
2023-09-24 18:00:58 +0530

    removed border

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -18,14 +18,11 @@ export default function Example() {
 
             <div className="relative isolate h-screen pt-0 mt-0 lg:px-8 bg-black pl-2 pr-2 w-full flex-1 ">
 
-                {/* <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 border"> */}
                 <div className="flex justify-center items-center h-full">
 
                     <div className="text-left w-3/5 md:mt-[-3rem]">
-                        {/* <h1 className="text-4xl font-bold tracking-tight bg-gradient-to-r from-[#8e5cf1]  to-[#dc0f94] inline-block text-transparent bg-clip-text  sm:text-6xl">
-                            Inspire Collaborate Innovate
-                        </h1> */}
-                         <div className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl space-y-5 font-extrabold flex border gap-5">
+                      
+                         <div className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl space-y-5 font-extrabold flex  gap-5">
 
                          <h1 className='text-white mt-4'>Inspire</h1>
                                 <div className="text-transparent bg-clip-text bg-gradient-to-r  from-purple-400 to-pink-600">
@@ -45,7 +42,7 @@ export default function Example() {
                                  
                         </div>
 
-                        <div className='border'>
+                        <div >
                             <p className="mt-6 font-mono text-2xl  leading-8 bg-gradient-to-r  text-white inline-block text-transparent bg-clip-text">
                                 Inspiring Minds
                             </p>
