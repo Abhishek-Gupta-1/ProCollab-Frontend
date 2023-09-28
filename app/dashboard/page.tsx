6870005044efc234560c2f2e7f426bce9c5aa760
Abhishek Gupta <abhisheknew2023@gmail.com>
2023-09-28 14:55:37 +0530

    changes in admin dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@
 import { getAllUniversityProject } from "@/api/apis";
 import { useEffect, useState } from "react"
 import Cookies from 'js-cookie'
-
+import Link from "next/link";
 
 
 const Page = () => {
@@ -25,31 +25,101 @@ const Page = () => {
   }
 
   return (
-    <div className="bg-black h-screen flex justify-center items-center">
-      <div className=" md:w-3/5 h-full flex justify-center  w-full ">
-        <div className=" h-fit  md:w-4/5 w-4/5 mt-7">
-          <div className="flex bg-white justify-between p-4 rounded-xl">
-            <p>Mahesh</p>
-            <p>Project title</p>
-            <button>Approve</button>
-            <button>Reject</button>
-          </div>
-          <div className="flex bg-white justify-between p-4 rounded-xl mt-5">
-            <p>Mahesh</p>
-            <p>Project title</p>
-            <button>Approve</button>
-            <button>Reject</button>
-          </div>
-          <div className="flex bg-white justify-between p-4 rounded-xl mt-5">
-            <p>Mahesh</p>
-            <p>Project title</p>
-            <button>Approve</button>
-            <button>Reject</button>
+    <div className="bg-black">
+      <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8 space-y-6">
+        {/* ============================================================= */}
+
+        <div className="justify-right text-right relative  bg-white px-6 sm:rounded-xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+
+          <div className="lg:pt-16 font-bold text-4xl text-left sm:pt-20">
+            <p>ProCollab</p>
           </div>
 
+          <div className=" relative mt-16 lg:h-32 md:h-8 sm:h-4 lg:mt-8 ml-20 md:w-full ">
+            <div className=" lg:space-x-12 lg:space-y-8 space-x-8 pb-8 mb-12">
+              <Link
+                href="/searchprojects"
+                className=" align-middle inline-block text-center bg-blue-500 shadow-lg px-8 py-2  rounded-md  text-sm font-semibold text-white "
+              >
+                Accept
+              </Link>
+
+              <Link
+                href="/searchprojects"
+                className=" text-align-middle inline-block  text-center bg-blue-500 px-8 py-2   rounded-md  text-sm font-semibold text-white "
+              >
+                Reject
+              </Link>
+            </div>
+            <div className="h-fit  md:w-4/5  mt-7">
+            </div>
+          </div>
         </div>
+        {/* ========================================================================== */}
+
+        {/* ============================================================= */}
+
+        <div className="justify-right text-right relative  bg-white px-6 sm:rounded-xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+
+          <div className="lg:pt-16 font-bold text-4xl text-left ">
+            <p>Upchar</p>
+          </div>
+
+          <div className="relative mt-16 lg:h-32 md:h-8 sm:h-4 lg:mt-8 ml-20 md:w-full ">
+            <div className=" lg:space-x-12 lg:space-y-8 space-x-8 pb-8 mb-12">
+              <Link
+                href="/searchprojects"
+                className=" align-middle inline-block text-center bg-blue-500 shadow-lg px-8 py-2  rounded-md  text-sm font-semibold text-white "
+              >
+                Accept
+              </Link>
+
+              <Link
+                href="/searchprojects"
+                className=" text-align-middle inline-block  text-center bg-blue-500 px-8 py-2   rounded-md  text-sm font-semibold text-white "
+              >
+                Reject
+              </Link>
+            </div>
+            <div className="h-fit  md:w-4/5  mt-7">
+            </div>
+          </div>
+        </div>
+        {/* ========================================================================== */}
+
+        {/* ============================================================= */}
+
+        <div className="justify-right text-right relative  bg-white px-6 sm:rounded-xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+
+          <div className="lg:pt-16 font-bold text-4xl text-left ">
+            <p>Assist</p>
+          </div>
+
+          <div className="relative mt-16 lg:h-32 md:h-8 sm:h-4 lg:mt-8 ml-20 md:w-full ">
+            <div className=" lg:space-x-12 lg:space-y-8 space-x-8 pb-8 mb-12">
+              <Link
+                href="/searchprojects"
+                className=" align-middle inline-block text-center bg-blue-500 shadow-lg px-8 py-2  rounded-md  text-sm font-semibold text-white "
+              >
+                Accept
+              </Link>
+
+              <Link
+                href="/searchprojects"
+                className=" text-align-middle inline-block  text-center bg-blue-500 px-8 py-2   rounded-md  text-sm font-semibold text-white "
+              >
+                Reject
+              </Link>
+            </div>
+            <div className="h-fit  md:w-4/5  mt-7">
+            </div>
+          </div>
+        </div>
+        {/* ========================================================================== */}
+
       </div>
     </div>
+
   )
 }
 
